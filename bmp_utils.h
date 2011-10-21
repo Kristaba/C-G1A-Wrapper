47a83c840bca4863a54af2f352dff8e894357e4f
Léo Grange <grangeleo@gmail.com>
2011-10-21 20:32:18 +0200

    add a README and the GNU GPL v3 headers

--- bmp_utils.h
+++ bmp_utils.h
@@ -1,3 +1,21 @@
+/**************************************************************************
+    This file is part of c_g1awrapper.
+
+    c_g1awrapper is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    c_g1awrapper is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with c_g1awrapper.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************/
+
+
 #ifndef _BMP_UTILS_H
 #define _BMP_UTILS_H
 
